ada1d5084f8f5e959fb1592789d5ad8e2dcded30
rogerio-stubs <rostubs17@gmail.com>
2021-03-14 03:31:09 -0300

    verificando a existência do elemento antes de observar alterações na tag

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-let elemento = document.querySelector('.inserir')
-let chats = document.querySelector('.elemento')
-
+setInterval(() => { 
+  if(document.querySelector('.elemento')){
+    let chats = document.querySelector('.elemento');
+    mutationObserver.observe(chats, {
+      attributes: true,
+      characterData: true,
+      childList: true,
+      subtree: true,
+      attributeOldValue: true,
+      characterDataOldValue: true
+    });
+  }
+  else {
+    console.log("Elemento não encontrado");
+  }
+ }, 1000)
 
 var mutationObserver = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-        let elemento = mutation.target.innerText;
-        console.log(elemento)
-    });
+  mutations.forEach(function(mutation) {
+    let elemento = mutation.target.innerText;
+    console.log('elemento', elemento);
   });
-
-mutationObserver.observe(chats, {
-    attributes: true,
-    characterData: true,
-    childList: true,
-    subtree: true,
-    attributeOldValue: true,
-    characterDataOldValue: true
-  });
+});
